bcdf7651ea4f2514abe01df47e8d1bf961b53a45
nahom mesfin <nahommesfin70@gmail.com>
2024-08-30 12:02:17 +0300

    hmm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+
+
 const App = () => {
   return (
-    <div className='bg-red-900 text-3xl '>App</div>
+    <div className='bg-slate-400 text-3xl text-center py-10 '>
+        <h1>BIKE SHARING PR</h1>
+    </div>
   )
 }
 
